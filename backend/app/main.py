c43c81af66e7b7d1f772d4ab0d44e6b6430bcacf
Victor7095 <victoryan345@gmail.com>
2023-08-12 14:45:40 -0400

    feat: Add CORS configurations

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,13 +1,19 @@
-from typing import Union
-
 from fastapi import FastAPI
-from fastapi.security import OAuth2PasswordBearer
+from fastapi.middleware.cors import CORSMiddleware
 from .models.database import create_db_and_tables
 from .router.api import api_router
 
 app = FastAPI()
 
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.on_event("startup")
