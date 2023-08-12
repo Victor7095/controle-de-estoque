1bc2c5f460022419ad208950bf49e58154bde7a1
Victor7095 <victoryan345@gmail.com>
2023-08-12 14:47:52 -0400

    refactor: Remove unnecessary files

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,30 +1,38 @@
 import { NgModule } from '@angular/core';
 import { HashLocationStrategy, LocationStrategy } from '@angular/common';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
+
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { AppLayoutModule } from './layout/app.layout.module';
+
 import { NotfoundComponent } from './demo/components/notfound/notfound.component';
+
 import { ProductService } from './demo/service/product.service';
-import { CountryService } from './demo/service/country.service';
-import { CustomerService } from './demo/service/customer.service';
-import { EventService } from './demo/service/event.service';
 import { IconService } from './demo/service/icon.service';
-import { NodeService } from './demo/service/node.service';
 import { CategoryService } from './demo/service/category.service';
+import { AuthService } from './demo/service/auth.service';
+import { AuthInterceptor } from './demo/service/auth.interceptor';
+import { environment } from 'src/environments/environment';
 
 @NgModule({
-    declarations: [
-        AppComponent, NotfoundComponent
-    ],
-    imports: [
-        AppRoutingModule,
-        AppLayoutModule
-    ],
+    declarations: [AppComponent, NotfoundComponent],
+    imports: [AppRoutingModule, AppLayoutModule],
     providers: [
         { provide: LocationStrategy, useClass: HashLocationStrategy },
-        CountryService, CustomerService, EventService, IconService, NodeService,
-        ProductService, CategoryService
+        AuthService,
+        IconService,
+        ProductService,
+        CategoryService,
+        {
+            provide: HTTP_INTERCEPTORS,
+            useClass: AuthInterceptor,
+            multi: true,
+        }, {
+            provide: 'BASE_API_URL',
+            useValue: environment.apiUrl
+        }
     ],
-    bootstrap: [AppComponent]
+    bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- frontend/src/app/demo/service/product.service.ts
+++ frontend/src/app/demo/service/product.service.ts
@@ -7,31 +7,17 @@ export class ProductService {
 
     constructor(private http: HttpClient) { }
 
-    getProductsSmall() {
-        return this.http.get<any>('assets/demo/data/products-small.json')
-            .toPromise()
-            .then(res => res.data as Product[])
-            .then(data => data);
+    async getProductsSmall() {
+        const res = await this.http.get<any>('assets/demo/data/products-small.json')
+            .toPromise();
+        const data = res.data as Product[];
+        return data;
     }
 
-    getProducts() {
-        return this.http.get<any>('assets/demo/data/products.json')
-            .toPromise()
-            .then(res => res.data as Product[])
-            .then(data => data);
-    }
-
-    getProductsMixed() {
-        return this.http.get<any>('assets/demo/data/products-mixed.json')
-            .toPromise()
-            .then(res => res.data as Product[])
-            .then(data => data);
-    }
-
-    getProductsWithOrdersSmall() {
-        return this.http.get<any>('assets/demo/data/products-orders-small.json')
-            .toPromise()
-            .then(res => res.data as Product[])
-            .then(data => data);
+    async getProducts() {
+        const res = await this.http.get<any>('assets/demo/data/products.json')
+            .toPromise();
+        const data = res.data as Product[];
+        return data;
     }
 }
